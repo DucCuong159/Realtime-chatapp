9c11277a1b672f7c00a888339ca03dfb9f304094
DucCuong159 <cuong.pd192732@gmail.com>
2026-08-16 22:13:20 +0700

    feat(frontend): connect auth state to route guard and initial auth status check in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,28 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Logo from "./components/logo";
+import { Spinner } from "./components/ui/spinner";
+import { useAuth } from "./hooks/use-auth";
 import AppRoutes from "./routes";
+import { isAuthRoute } from "./routes/routes";
 
 function App() {
+  const { pathname } = useLocation();
+  const { user, isAuthStatus, isAuthStatusLoading } = useAuth();
+  const isAuth = isAuthRoute(pathname);
+
+  useEffect(() => {
+    isAuthStatus();
+  }, [isAuthStatus]);
+
+  if (isAuthStatusLoading && !user && !isAuth)
+    return (
+      <div className="flex flex-col items-center justify-center h-screen w-screen">
+        <Logo imgClass="size-20" />
+        <Spinner className="w-6 h-6" />
+      </div>
+    );
+
   return <AppRoutes />;
 }
 
