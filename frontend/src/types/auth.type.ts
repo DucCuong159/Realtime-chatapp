e771d1f7e62e9e0aaf02e6a3e698c310d90a65ff
DucCuong159 <cuong.pd192732@gmail.com>
2026-08-16 22:12:13 +0700

    feat(frontend): add auth types and zod validation schemas

---
+++ frontend/src/types/auth.type.ts
@@ -0,0 +1,22 @@
+export type RegisterType = {
+  name: string;
+  email: string;
+  password: string;
+  avatar?: string;
+  about?: string;
+};
+
+export type LoginType = {
+  email: string;
+  password: string;
+};
+
+export type UserType = {
+  _id: string;
+  name: string;
+  email: string;
+  avatar?: string | null;
+  about?: string | null;
+  createdAt: string;
+  updatedAt: string;
+};
